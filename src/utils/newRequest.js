b6863d63bb9b73681a04879eba3c00eda899bdd7
Naya O <naya.onwubiko@gmail.com>
2024-04-04 19:14:28 -0400

    modified api server

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL:"http://localhost:8800/api/",
-    withCredentials: true,
+  baseURL: "https://freelancer-api-o9p0.onrender.com/api",
+  withCredentials: true,
 });
 
 export default newRequest;
