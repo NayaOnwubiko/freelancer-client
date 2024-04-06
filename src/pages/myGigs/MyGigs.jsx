b3a85aec91bba6883dbff84a07b62f38badb04b4
Naya O <naya.onwubiko@gmail.com>
2024-04-05 23:56:12 -0400

    modified mygigs page

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./MyGigs.scss";
 import { Link } from "react-router-dom";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import getCurrentUser from "../../utils/getCurrentUser.js";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
 
-const MyGigs = () => {
-  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+const myGigs = () => {
+  const currentUser = getCurrentUser();
 
   const queryClient = useQueryClient();
 
   const { isLoading, error, data } = useQuery({
-    queryKey: ["myGigs"],
+    queryKey: ["mygigs"],
     queryFn: () =>
       newRequest.get(`/gigs?userId=${currentUser._id}`).then((res) => {
         return res.data;
@@ -22,7 +23,7 @@ const MyGigs = () => {
       return newRequest.delete(`/gigs/${id}`);
     },
     onSuccess: () => {
-      queryClient.invalidateQueries(["myGigs"]);
+      queryClient.invalidateQueries(["mygigs"]);
     },
   });
 
@@ -33,45 +34,45 @@ const MyGigs = () => {
   return (
     <div className="myGigs">
       {isLoading ? (
-        "loading"
+        "Loading"
       ) : error ? (
-        "error"
+        "Something went Wrong"
       ) : (
         <div className="container">
           <div className="title">
             <h1>Gigs</h1>
-            {currentUser.isSeller && (
-              <Link to="/add">
-                <button>Add New Gig</button>
-              </Link>
-            )}
+            <Link className="link" to="/add">
+              <button>Add New Gig</button>
+            </Link>
           </div>
           <table>
-            <tr>
-              <th>Image</th>
-              <th>Title</th>
-              <th>Price</th>
-              <th>Sales</th>
-              <th>Action</th>
-            </tr>
-            {data.map((gig) => (
-              <tr key={gig._id}>
-                <td>
-                  <img className="img" src={gig.cover} alt="" />
-                </td>
-                <td>{gig.title}</td>
-                <td>{gig.price}</td>
-                <td>{gig.sales}</td>
-                <td>
-                  <img
-                    className="delete"
-                    src="/img/delete.png"
-                    alt=""
-                    onClick={() => handleDelete(gig._id)}
-                  />
-                </td>
+            <tbody>
+              <tr>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Sales</th>
+                <th>Action</th>
               </tr>
-            ))}
+              {data.map((gig) => (
+                <tr key={gig._id}>
+                  <td>
+                    <img className="image" src={gig?.cover} alt="" />
+                  </td>
+                  <td>{gig?.title}</td>
+                  <td>$ {gig?.price}</td>
+                  <td>{gig?.sales}</td>
+                  <td>
+                    <img
+                      className="delete"
+                      src="/img/delete.png"
+                      alt=""
+                      onClick={() => handleDelete(gig._id)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       )}
@@ -79,4 +80,4 @@ const MyGigs = () => {
   );
 };
 
-export default MyGigs;
+export default myGigs;
